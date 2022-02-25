9f045f86d812efc91ef080c2c1940b041b6e499d
Elbert Chan <elb.chan@gmail.com>
2022-02-25 12:09:15 -0800

    functional is complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,26 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    if (Object.keys(storage).length === 0) {
+      storage[0] = value;
+    } else {
+      storage[Math.max(...Object.keys(storage)) + 1] = value;
+    }
   };
 
   someInstance.dequeue = function() {
+    if (Object.keys(storage).length === 0) {
+      console.log('nothing lol');
+    } else {
+      let valToReturn = storage[Math.min(...Object.keys(storage))];
+      delete storage[Math.min(...Object.keys(storage))];
+      return valToReturn;
+    }
   };
 
   someInstance.size = function() {
+    console.log(Object.keys(storage));
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,26 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    if (Object.keys(storage).length === 0) {
+      storage[0] = value;
+    } else {
+      storage[Math.max(...Object.keys(storage)) + 1] = value;
+    }
   };
 
   someInstance.pop = function() {
+    if (Object.keys(storage).length === 0) {
+      console.log('nothing lol');
+    } else {
+      let valToReturn = storage[Math.max(...Object.keys(storage))];
+      delete storage[Math.max(...Object.keys(storage))];
+      return valToReturn;
+    }
   };
 
   someInstance.size = function() {
+    console.log(Object.keys(storage));
+    return Object.keys(storage).length;
   };
 
   return someInstance;
